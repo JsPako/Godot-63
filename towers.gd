cc1ab76ded22270c7bfde11517ed9789598b87bd
Jakub <36249011+ItsRommels@users.noreply.github.com>
2023-11-11 14:34:06 +0000

    addednonfunctowers

---
+++ towers.gd
@@ -0,0 +1,14 @@
+extends StaticBody2D
+
+var Bullet = preload("res://Assets/towerDefense_tile275.png")
+var BulletDamage = 5
+var pathName
+var currTargets = []
+var curr
+
+func _on_tower_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_tower_body_exited(body):
+	pass # Replace with function body.
