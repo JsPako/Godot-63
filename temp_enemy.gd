bb7b3647c10f1a612326e1fa98268840194d100e
Jacob Swiniarski <jacobswiniarski@gmail.com>
2023-11-12 15:53:23 +0000

    Update Fix

--- temp_enemy.gd
+++ temp_enemy.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
-@export var speed = 100
+var speed = 100
+var damage = 1
+var health = 10
 
 func _process(delta):
 	get_parent().set_progress(get_parent().get_progress() + speed * delta)
 	if get_parent().get_progress_ratio() > 0.99:
-		GameState.health = GameState.health - 1
+		GameState.health = GameState.health - damage
+		get_parent().get_parent().queue_free()
+	
+	if health == 0:
 		get_parent().get_parent().queue_free()
